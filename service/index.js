2a4e37932f0a7a93641489ce0e2e0673abc7b5cd
Aundra Miller <miller.aundra@gmail.com>
2022-09-24 11:12:35 -0700

    Find images assets and send back paths in get request

--- service/index.js
+++ service/index.js
@@ -1,1 +1,26 @@
-console.log("hello world");
+const fs = require("fs/promises");
+const express = require("express");
+const cors = require("cors");
+const _ = require("lodash");
+const { v4: uuid } = require ("uuid");
+
+const app = express();
+
+app.get("/avatars", (req, res) => {
+    res.json({
+        "Russel": {
+            "url": "images/dino/Idle (1).png"
+        },
+        "Tank": {
+            "url": "images/robot/Idle (1).png"
+        },
+        "Stella": {
+            "url": "images/ninja/Idle__000.png"
+        }
+    });
+});
+
+app.listen(3000, () => {
+    console.log('api server is running...');
+});
+
